7c78a94d6aaa16274b85e8cab92027e98e87e29e
EMNGz <gameball326@gmail.com>
2024-01-31 15:12:24 +0700

    first commit

---
+++ table.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.3.2/css/all.min.css" integrity="sha384-xZQk8J6Hbe7WNfA5d+J+26W+5j8wz5+U2TTCKbO+5aF8gBsf5zI+IkA6IHvmKp2" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body>
+<div class="row">
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th scope="col" colspan="8" >อัตรากำลังพล</br>
+        ฝ่ายพิธีการเข้าเมือง ตม.ทอ.สุวรรณภูมิ</th>
+      </tr>  
+    </thead>
+    <tbody>
+      <tr >
+        <th scope="row">ตำแหน่ง</th>
+        <th>ยอดเต็มอัตรา </th>
+        <th>คนครอง</th>
+        <th>รวม(คนครอง)</th>
+        <th>ว่าง</th>
+        <th>มีตัวมาปฏิบัติงานจริง</th>
+        <th>ไปช่วยราชการ</th>
+        <th>มาช่วยราชการ</th>
+      </tr>
+      <tr >
+        <th scope="row">ผกก</th>
+        <th>1 </th>
+        <th>1</th>
+        <th>1</th>
+        <th>0</th>
+        <th>1</th>
+        <th>-</th>
+        <th>-</th>
+      </tr>
+      <tr >
+        <th scope="row">รอง ผกก.</th>
+        <th>4 </th>
+        <th>4</th>
+        <th>4</th>
+        <th>0</th>
+        <th>4</th>
+        <th>-</th>
+        <th>-</th>
+      </tr>
+      <tr >
+        <th scope="row">สว.</th>
+        <th>7 </th>
+        <th>7</th>
+        <th>7</th>
+        <th>0</th>
+        <th>7</th>
+        <th>-</th>
+        <th>-</th>
+      </tr>
+      <tr >
+        <th scope="row">รอง สว.</th>
+        <th>46</th>
+        <th>27</th>
+        <th>27</th>
+        <th>19</th>
+        <th>27</th>
+        <th>1</th>
+        <th>1</th>
+      </tr>
+      <tr >
+        <th scope="row">รอง สว.(อก)</th>
+        <th rowspan="2" >134</th>
+        <th>7</th>
+        <th rowspan="2">75</th>
+        <th rowspan="2">58</th>
+        <th>7</th>
+        <th>-</th>
+        <th>-</th>
+      </tr>
+      <tr >
+        <th scope="row">ผบ.หมู่</th>       
+        <th>68</th>
+        <th>68</th>
+        <th>-</th>
+        <th>-</th>
+      </tr>
+      <tr >
+        <th scope="row">รวมทั้งสิ้น</th>
+        <th>192</th>
+        <th>114</th>
+        <th>114</th>
+        <th>78</th>
+        <th>114</th>
+        <th>1</th>
+        <th>1</th>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</body>
+</html>
+
+<style>
+.table th, /*ปรับให้ข้อความในตารางอยู่ตรงกลาง */ 
+.table td {
+  text-align: center;
+  border:1px solid black;
+}
+.table th[rowspan],
+    .table td[rowspan] {
+      vertical-align: middle;
+    }
+</style>
