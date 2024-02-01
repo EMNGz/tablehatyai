00e933719e69de1b81953cfc6d09c123eb90863f
EMNGz <gameball326@gmail.com>
2024-02-01 18:03:16 +0700

    first commit

--- table.php
+++ table.php
@@ -13,87 +13,178 @@
   <table class="table table-bordered">
     <thead>
       <tr>
-        <th scope="col" colspan="8" >อัตรากำลังพล</br>
-        ฝ่ายพิธีการเข้าเมือง ตม.ทอ.สุวรรณภูมิ</th>
+        <th scope="col" colspan="6" >บัญชีข้าราชการตำรวจ ด่าน ตม.ทอ.หาดใหญ่ </br>
+        วันที่ 18 มกราคม พ.ศ.2567 </th>
       </tr>  
     </thead>
     <tbody>
       <tr >
-        <th scope="row">ตำแหน่ง</th>
-        <th>ยอดเต็มอัตรา </th>
-        <th>คนครอง</th>
-        <th>รวม(คนครอง)</th>
-        <th>ว่าง</th>
-        <th>มีตัวมาปฏิบัติงานจริง</th>
-        <th>ไปช่วยราชการ</th>
-        <th>มาช่วยราชการ</th>
+        <th scope="row" >ลำดับ</th>
+        <th >ยศ </th>
+        <th >ชื่อ</th>
+        <th >สกุล</th>
+        <th >ตำแหน่ง</th>
+        <th>หมายเหตุ</th>
       </tr>
       <tr >
-        <th scope="row">ผกก</th>
-        <th>1 </th>
-        <th>1</th>
-        <th>1</th>
-        <th>0</th>
-        <th>1</th>
-        <th>-</th>
-        <th>-</th>
+        <th scope="row" >1</th>		  	 	
+        <th >พ.ต.ท. </th>
+        <th >สราวุธ</th>
+        <th >นพกาศ</th>
+        <th >สว.ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
       </tr>
       <tr >
-        <th scope="row">รอง ผกก.</th>
-        <th>4 </th>
-        <th>4</th>
-        <th>4</th>
-        <th>0</th>
-        <th>4</th>
-        <th>-</th>
-        <th>-</th>
+        <th scope="row" >2</th>			
+        <th >ร.ต.อ.หญิง </th>
+        <th >วันเพ็ญ</th>
+        <th >ซ้วนเล้ง</th>
+        <th >รอง สว.ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
       </tr>
       <tr >
-        <th scope="row">สว.</th>
-        <th>7 </th>
-        <th>7</th>
-        <th>7</th>
-        <th>0</th>
-        <th>7</th>
-        <th>-</th>
-        <th>-</th>
+        <th scope="row" >3</th>		  	 		 		
+        <th >ร.ต.อ.หญิง</th>
+        <th >สุไก่</th>
+        <th >โสบผอม</th>
+        <th >รอง สว.ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
       </tr>
       <tr >
-        <th scope="row">รอง สว.</th>
-        <th>46</th>
-        <th>27</th>
-        <th>27</th>
-        <th>19</th>
-        <th>27</th>
-        <th>1</th>
-        <th>1</th>
+        <th scope="row" >4</th>
+        <th >ร.ต.อ. </th>	 	 	
+        <th >กฤดกานต์</th>
+        <th >กฤษณะพันธ์</th>
+        <th >รอง สว.ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
       </tr>
       <tr >
-        <th scope="row">รอง สว.(อก)</th>
-        <th rowspan="2" >134</th>
-        <th>7</th>
-        <th rowspan="2">75</th>
-        <th rowspan="2">58</th>
-        <th>7</th>
-        <th>-</th>
-        <th>-</th>
+        <th scope="row" >5</th>
+        <th >ร.ต.อ. </th>	 	 	
+        <th >กฤศณัฏฐ์   </th>
+        <th >คงยัง</th>
+        <th >รอง สว.ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
       </tr>
       <tr >
-        <th scope="row">ผบ.หมู่</th>       
-        <th>68</th>
-        <th>68</th>
-        <th>-</th>
-        <th>-</th>
+        <th scope="row" >6</th>
+        <th >ร.ต.อ.หญิง </th>	 	 	
+        <th >อาภา   </th>
+        <th >สุวรรณยุหะ</th>
+        <th >รอง สว.(อก) ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
       </tr>
       <tr >
-        <th scope="row">รวมทั้งสิ้น</th>
-        <th>192</th>
-        <th>114</th>
-        <th>114</th>
-        <th>78</th>
-        <th>114</th>
-        <th>1</th>
-        <th>1</th>
+        <th scope="row" >7</th>
+        <th >ด.ต. </th>	 	 	
+        <th >สัญญา   </th>
+        <th >สุขนุ้ย</th>
+        <th >ผบ.หมู่ ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
+      </tr>
+      <tr >
+        <th scope="row" >8</th>
+        <th >ด.ต. </th>	 	 	
+        <th >อภิสิทธิ์   </th>
+        <th >สิทธิพันธ์</th>
+        <th >ผบ.หมู่ ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
+      </tr>
+      <tr>
+        <th scope="row" >9</th>
+        <th >ด.ต. </th>	 	 	
+        <th >อนุศรณ์ </th>
+        <th >เรืองแก้ว </th>
+        <th >ผบ.หมู่ ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
+      </tr>
+      <tr>
+        <th scope="row" >10</th>
+        <th >ด.ต. </th>	 	 	
+        <th >เอกดนัย</th>
+        <th >จิรวัฒน์</th>
+        <th >ผบ.หมู่ ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
+      </tr>
+      <tr>
+        <th scope="row" >11</th>
+        <th >ด.ต. </th>	 	 	
+        <th >นนท์ธิวรรธน์</th>
+        <th >จินดาสุวรรณ์</th>
+        <th >ผบ.หมู่ ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
+      </tr>
+      <tr>
+        <th scope="row" >12</th>
+        <th >ด.ต.หญิง </th>	 	 	
+        <th >ปรียาภรณ์ </th>
+        <th >สะอาดจันทร์</th>
+        <th >ผบ.หมู่ ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
+      </tr>
+      <tr>
+        <th scope="row" >13</th>
+        <th >ด.ต.</th>	 	 	
+        <th >จิรกร</th>
+        <th >หลีหาด</th>
+        <th >ผบ.หมู่ ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
+      </tr>
+      <tr>
+        <th scope="row" >14</th>
+        <th >ด.ต.หญิง</th>	 	 	
+        <th >นิตยา</th>
+        <th >เสาวนิตย์</th>
+        <th >ผบ.หมู่ ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
+      </tr>
+      <tr>
+        <th scope="row" >15</th>
+        <th >ด.ต.</th>	 	 	
+        <th >อภิศักดิ์</th>
+        <th >จงรักษ์</th>
+        <th >ผบ.หมู่ ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
+      </tr>
+      <tr>
+        <th scope="row" >16</th>
+        <th >ด.ต.</th>	 	 	
+        <th >จรณชัย </th>
+        <th >ยางทอง </th>
+        <th >ผบ.หมู่ ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
+      </tr>
+      <tr>
+        <th scope="row" >17</th>
+        <th >ด.ต.</th>	 	 	
+        <th >จำลอง  </th>
+        <th >คงแก้ว </th>
+        <th >ผบ.หมู่ ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
+      </tr>
+      <tr>
+        <th scope="row" >18</th>
+        <th >ด.ต.หญิง</th>	 	 	
+        <th >สายชล</th>
+        <th >เทพทอง</th>
+        <th >ผบ.หมู่ ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
+      </tr>
+      <tr>
+        <th scope="row" >19</th>
+        <th >ส.ต.อ.</th>	 	 	
+        <th >ฟาร์อิซซุดดีน</th>
+        <th >อนุสามัญสกุล</th>
+        <th >ผบ.หมู่ ด่าน ตม.ทอ.หาดใหญ่</th>
+        <th></th>
+      </tr>
+      <tr>
+        <th scope="row"  rowspan="2">20</th>
+        <th rowspan="2">ส.ต.อ.หญิง</th>	 	 	
+        <th rowspan="2">อธิชา  </th>
+        <th rowspan="2">ศิริไชย </th>
+        <th rowspan="2">ผบ.หมู่ ด่าน ตม.ทอ.ภูเก็ต ปรก. <br>ผบ.หมู่ ด่าน ตม.ทอ.หาดใหญ่ </br></th>
+        <th rowspan="2"></th>
       </tr>
     </tbody>
   </table>
